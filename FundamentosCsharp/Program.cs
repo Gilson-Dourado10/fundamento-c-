8c0f078f768a4208099e4d531b9d5ad9f6fcf759
Gilson-Dourado10 <gilsondourado10@gmail.com>
2022-02-18 06:37:32 -0300

    fundamentos c#

---
+++ FundamentosCsharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FundamentosCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuMetodo(" C# É MUITO LEGAL  ");
+            var nome = RetornaNome("Gilson", "Dourado");
+             Console.WriteLine(nome);
+        }
+
+        static void MeuMetodo(string parametro)
+        {
+            Console.WriteLine(parametro);
+        }
+
+        static string RetornaNome
+        (
+            string nome,
+            string sobrenome,
+            int idade = 35
+        )
+        {
+            return (nome + " " + sobrenome + " tem " + idade.ToString() + " anos");
+        }
+
+    }
+
+}
