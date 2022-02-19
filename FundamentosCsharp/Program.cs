61e6717b6d523997dd84f4a16a81c0e0ffc558d6
Gilson-Dourado10 <gilsondourado10@gmail.com>
2022-02-19 07:21:20 -0300

    macrescentando  ais métodos

--- FundamentosCsharp/Program.cs
+++ FundamentosCsharp/Program.cs
@@ -8,24 +8,30 @@ namespace FundamentosCsharp
         {
             MeuMetodo(" C# É MUITO LEGAL  ");
             var nome = RetornaNome("Gilson", "Dourado");
-             Console.WriteLine(nome);
+            Console.WriteLine(nome);
+            
+            var resultado = SomaDouble(20.5, 3.5);
+            Console.WriteLine("A soma do resultado é: " + resultado);
+            
         }
 
         static void MeuMetodo(string parametro)
         {
             Console.WriteLine(parametro);
+            
         }
-
-        static string RetornaNome
-        (
-            string nome,
-            string sobrenome,
-            int idade = 35
-        )
+        static string RetornaNome(string nome, string sobrenome, int idade = 36)
         {
             return (nome + " " + sobrenome + " tem " + idade.ToString() + " anos");
         }
-
+        static double SomaDouble(double var1, double var2)
+        {
+            return (var1) + (var2);
+        }
+        static void EscrevaLinha(string linha)
+        {
+            Console.WriteLine(linha);
+            EscrevaLinha("********---------------***********");
+        }
     }
-
 }
